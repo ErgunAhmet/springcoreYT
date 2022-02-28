504a119fc1056aceeb76c5f6b1b0a3b54d60ce34
ErgunAhmet <AhmetErgun2000@protonmail.com>
2022-02-28 15:09:17 +0100

    initial commit

---
+++ javaconfig/src/main/java/com/springcore/javaconfig/JavaconfigApplication.java
@@ -0,0 +1,11 @@
+package com.springcore.javaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaconfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaconfigApplication.class, args);
+    }
+}
